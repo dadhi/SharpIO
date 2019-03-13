ab0c195b513ada47365bb0a568992970a9c9119d
Maksim Volkau <maksim.volkau@adform.com>
2019-03-13 17:55:35 +0300

    IO simplified to a delegate; BUT unable to select from multiple SelectMany converting from CannotFail to/from TErr

--- SharpIO/SharpIO.cs
+++ SharpIO/SharpIO.cs
@@ -3,6 +3,7 @@ namespace SharpIODemo
     using System;
     using SharpIOLib;
     using static SharpIOLib.Empty;
+    using static SharpIOLib.Result;
     using static SharpIOLib.SharpIO;
 
     public class Program
@@ -12,12 +13,13 @@ namespace SharpIODemo
             Console.WriteLine("🌄");
         }
 
-        public static IO<R, Exception, Empty> GetProgram<R>()
-            where R : ILog<R>, IConsole<R> =>
-            from logStart in Log.Info<R>("Starting the quiz...")
-            from askForName in ConsoleX.WriteLine<R>("Enter your name:")
-            from name in ConsoleX.ReadLine<R>()
-            //from logName in Log.Info<R>($"Name is {name}")
+        public static IO<E, Exception, Empty> GetProgram<E>()
+            where E : ILog<E>, IConsole<E> =>
+            from logStart in Log.Info<E>("Starting the quiz...")
+            from askForName in ConsoleX.WriteLine<E>("Enter your name:")
+            from name in ConsoleX.ReadLine<E>()
+            //from logName in Log.Info<E>($"Name is @{name}")
+            //from writeName in ConsoleX.WriteLine<E>($"Name is {name}") 
             select empty;
     }
 
@@ -29,13 +31,11 @@ namespace SharpIODemo
 
     public static class ConsoleX
     {
-        public static IO<R, CannotFail, Empty> WriteLine<R>(string line)
-            where R : IConsole<R> =>
-            Runtime<R, CannotFail>().Bind(r => r.WriteLine(line));
+        public static IO<E, CannotFail, Empty> WriteLine<E>(string line) where E : IConsole<E> =>
+            Env<E>().BindFromCannotFail(x => x.WriteLine(line));
 
-        public static IO<R, Exception, string> ReadLine<R>()
-            where R : IConsole<R> =>
-            Runtime<R, Exception>().Bind(r => r.ReadLine());
+        public static IO<E, Exception, string> ReadLine<E>() where E : IConsole<E> =>
+            Env<E, Exception>().Bind(x => x.ReadLine());
     }
 
     public interface ILog<in _>
@@ -45,22 +45,20 @@ namespace SharpIODemo
 
     public static class Log
     {
-        public static IO<R, CannotFail, Empty> Info<R>(string message)
-            where R : ILog<R> =>
-            Runtime<R, CannotFail>().Bind(c => c.Info(message));
+        public static IO<R, CannotFail, Empty> Info<R>(string message) where R : ILog<R> =>
+            Env<R>().BindFromCannotFail(x => x.Info(message));
     }
 
     public class TestConsole<_> : IConsole<_>
     {
-        public IO<_, CannotFail, Empty> WriteLine(
-            string line) => empty.ToIO<_, CannotFail>();
-        public IO<_, Exception, string> ReadLine() =>
-            "hey".ToIO<_, Exception, string>();
+        public IO<_, CannotFail, Empty> WriteLine(string line) => Empty<_>();
+
+        public IO<_, Exception, string> ReadLine() => Success("hey", default(Exception)).ToIO; //IO<_, Exception, string>("hey");
     }
 
     public class TestLog<_> : ILog<_>
     {
-        public IO<_, CannotFail, Empty> Info(string message) => empty.ToIO<_, CannotFail>();
+        public IO<_, CannotFail, Empty> Info(string message) => Empty<_>();
     }
 }
 
@@ -68,104 +66,117 @@ namespace SharpIOLib
 {
     using System;
     using static Empty;
+    using static Result;
+
+    // ReSharper disable UnusedTypeParameter
 
     public abstract class CannotFail
     {
         private CannotFail() { }
     }
 
-    public class Empty
+    public readonly struct Empty
     {
         public static readonly Empty empty = new Empty();
     }
 
-    public interface IO<in R, out E, out A>
+    public delegate IResult<TErr, TVal> IO<in TEnv, out TErr, out TVal>(TEnv env);
+
+    //public interface IO<in R, out E, out A>
+    //{
+    //    IResult<E, A> Run(R runtime);
+    //}
+
+    //public struct SIO<R, E, A> : IO<R, E, A>
+    //{
+    //    public Func<R, IResult<E, A>> Runner;
+    //    public SIO(Func<R, IResult<E, A>> runner) => Runner = runner;
+    //    public IResult<E, A> Run(R runtime) => Runner(runtime);
+    //}
+
+    public interface IResult<out TErr, out TVal> { }
+
+    public struct Success<TErr, TVal> : IResult<TErr, TVal>
     {
-        IResult<E, A> Run(R runtime);
+        public readonly TVal Value;
+        public Success(TVal value) => Value = value;
     }
 
-    public struct SIO<R, E, A> : IO<R, E, A>
+    public struct Failure<TErr, TVal> : IResult<TErr, TVal>
     {
-        public Func<R, IResult<E, A>> Runner;
-        public SIO(Func<R, IResult<E, A>> runner) => Runner = runner;
-        public IResult<E, A> Run(R runtime) => Runner(runtime);
+        public readonly TErr Error;
+        public Failure(TErr error) => Error = error;
+
+        public Failure<TErr, TOther> FailFor<TOther>() => new Failure<TErr, TOther>(Error);
     }
 
-    public interface IResult<out E, out A> { }
-
-    public struct Success<E, A> : IResult<E, A>
+    public static class Result
     {
-        public readonly A Value;
-        public Success(A a) => Value = a;
+        public static IResult<TErr, TVal> Success<TErr, TVal>(TVal value, TErr _ = default) => new Success<TErr, TVal>(value);
+
+        public static IResult<TErr, Empty> Success<TErr>() => new Success<TErr, Empty>(empty);
+
+        public static IResult<CannotFail, TVal> Success<TVal>(TVal value) => new Success<CannotFail, TVal>(value);
+
+        public static IResult<CannotFail, Empty> Success() => SuccessEmpty;
+        public static readonly Success<CannotFail, Empty> SuccessEmpty = new Success<CannotFail, Empty>(empty);
+
+        public static IResult<TErr, TVal> Failure<TErr, TVal>(TErr error, TVal _ = default) => new Failure<TErr, TVal>(error);
+
+        public static IResult<TErr, Empty> Failure<TErr>(TErr error) => new Failure<TErr, Empty>(error);
+
+        public static IResult<TErr, B> Map<TErr, A, B>(this IResult<TErr, A> a, Func<A, B> map) =>
+            a is Success<TErr, A> success ? Success<TErr, B>(map(success.Value)) : ((Failure<TErr, A>)a).FailFor<B>();
     }
 
-    public struct Failure<E, A> : IResult<E, A>
-    {
-        public readonly E Error;
-        public Failure(E e) => Error = e;
-        public Failure<E, B> To<B>() => new Failure<E, B>(Error);
-    }
-
+    // todo: rename to IO or EnvIO
     public static class SharpIO
     {
-        public static IResult<E, A> Success<E, A>(
-            A a, E _ = default) => new Success<E, A>(a);
+        public static IResult<TErr, TVal> ToIO<TEnv, TErr, TVal>(this IResult<TErr, TVal> result, TEnv _ = default) => result;
 
-        public static Success<CannotFail, A> Success<A>(
-            A a) => new Success<CannotFail, A>(a);
+        public static IO<TEnv, TErr, TVal> ToIO<TEnv, TErr, TVal>(this TVal value, TErr _ = default, TEnv __ = default) => Success(value, default(TErr)).ToIO;
 
-        public static IResult<E, A> Failure<E, A>(
-            E e, A _ = default) => new Failure<E, A>(e);
+        public static IO<TEnv, TErr, Empty> Empty<TEnv, TErr>() => Success<TErr>().ToIO;
 
-        public static IResult<E, A> RunIO<R, E, A>(
-            this IResult<E, A> a, R runtime = default) => a;
+        public static IO<TEnv, CannotFail, Empty> Empty<TEnv>() => Success().ToIO;
 
-        public static IO<R, E, A> ToIO<R, E, A>(
-            this A a, E _ = default, R __ = default) =>
-            new SIO<R, E, A>(Success(a, _).RunIO);
+        public static IO<TEnv, TErr, TEnv> Env<TEnv, TErr>() => env => Success(env, default(TErr));
 
-        public static IO<R, E, Empty> ToIO<R, E>(
-            this Empty none, R runtime = default) =>
-            new SIO<R, E, Empty>(Success(empty, default(E)).RunIO);
+        public static IO<TEnv, CannotFail, TEnv> Env<TEnv>() => Success;
 
-        public static IO<R, E, R> Runtime<R, E>(E _ = default) =>
-            new SIO<R, E, R>(r => Success(r, default(E)));
+        public static IO<TEnv, TErr, B> Map<TEnv, TErr, A, B>(this IO<TEnv, TErr, A> a, Func<A, B> map) => env => a(env).Map(map);
 
-        public static IResult<E, B> To<E, A, B>(
-            this IResult<E, A> a, Func<A, B> map) =>
-            a is Success<E, A> suc
-            ? Success<E, B>(map(suc.Value))
-            : ((Failure<E, A>)a).To<B>();
-
-        public static IO<R, E, B> To<R, E, A, B>(
-            this IO<R, E, A> a, Func<A, B> map) =>
-            new SIO<R, E, B>(r => a.Run(r).To(map));
-
-        public static IO<R, E, B> Bind<R, E, A, B>(
-            this IO<R, E, A> a, Func<A, IO<R, E, B>> map) =>
-            new SIO<R, E, B>(r =>
+        public static IO<TEnv, TErr, B> Bind<TEnv, TErr, A, B>(this IO<TEnv, TErr, A> a, Func<A, IO<TEnv, TErr, B>> map) =>
+            env =>
             {
-                var x = a.Run(r);
-                return x is Success<E, A> suc
-                    ? map(suc.Value).Run(r)
-                    : ((Failure<E, A>)x).To<B>().RunIO(r);
-            });
+                var result = a(env);
+                return result is Success<TErr, A> success ? map(success.Value)(env) : ((Failure<TErr, A>)result).FailFor<B>().ToIO(env);
+            };
 
-        public static IO<R, E, B> BindCannotFail<R, E, A, B>(
-          this IO<R, CannotFail, A> a, Func<A, IO<R, E, B>> map) =>
-          new SIO<R, E, B>(r => map(((Success<CannotFail, A>)a.Run(r)).Value).Run(r));
+        public static IO<TEnv, TErr, B> BindFromCannotFail<TEnv, TErr, A, B>(this IO<TEnv, CannotFail, A> a, Func<A, IO<TEnv, TErr, B>> map) =>
+          env => map(((Success<CannotFail, A>)a(env)).Value)(env);
 
-        public static IO<R, E, B> BindToCannotFail<R, E, A, B>(
-            this IO<R, CannotFail, A> a, Func<A, IO<R, E, B>> map) =>
-            new SIO<R, E, B>(r => map(((Success<CannotFail, A>)a.Run(r)).Value).Run(r));
+        public static IO<TEnv, TErr, B> BindToCannotFail<TEnv, TErr, A, B>(this IO<TEnv, TErr, A> a, Func<A, IO<TEnv, CannotFail, B>> map) =>
+            env =>
+            {
+                var result = a(env);
+                return result is Success<TErr, A> success
+                    ? Success<TErr, B>(((Success<CannotFail, B>)map(success.Value)(env)).Value)
+                    : ((Failure<TErr, A>)result).FailFor<B>().ToIO(env);
+            };
+    }
 
-        public static IO<R, E, B> Select<R, E, A, B>(this IO<R, E, A> a, Func<A, B> map) =>
-            a.To(map);
+    public static class LinqIOExtensions
+    {
+        public static IO<TEnv, TErr, B> Select<TEnv, TErr, A, B>(this IO<TEnv, TErr, A> a, Func<A, B> map) => a.Map(map);
 
-        public static IO<R, E, C> SelectMany<R, E, A, B, C>(this IO<R, E, A> a, Func<A, IO<R, E, B>> map, Func<A, B, C> project) =>
-            a.Bind(x => map(x).Bind(y => project(x, y).ToIO<R, E, C>()));
+        public static IO<TEnv, TErr, C> SelectMany<TEnv, TErr, A, B, C>(this IO<TEnv, TErr, A> a, Func<A, IO<TEnv, TErr, B>> map, Func<A, B, C> project) =>
+            a.Bind(x => map(x).Bind(y => project(x, y).ToIO<TEnv, TErr, C>()));
 
-        public static IO<R, E, C> SelectMany<R, E, A, B, C>(this IO<R, CannotFail, A> a, Func<A, IO<R, E, B>> map, Func<A, B, C> project) =>
-            a.BindCannotFail(x => map(x).Bind(y => project(x, y).ToIO<R, E, C>()));
+        public static IO<TEnv, TErr, C> SelectMany<TEnv, TErr, A, B, C>(this IO<TEnv, CannotFail, A> a, Func<A, IO<TEnv, TErr, B>> map, Func<A, B, C> project) =>
+            a.BindFromCannotFail(x => map(x).Bind(y => project(x, y).ToIO<TEnv, TErr, C>()));
+
+        //public static IO<TEnv, TErr, C> SelectMany<TEnv, TErr, A, B, C>(this IO<TEnv, TErr, A> a, Func<A, IO<TEnv, CannotFail, B>> map, Func<A, B, C> project) =>
+        //    a.BindToCannotFail(x => map(x).Bind(y => project(x, y).ToIO<TEnv, CannotFail, C>()));
     }
 }
